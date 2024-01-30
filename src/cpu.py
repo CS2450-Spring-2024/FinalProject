78a62f9b81d5a7532ac3596cb868a1024e07b662
Noah Christensen <mcnbc12@gmail.com>
2024-01-30 22:42:47 +0000

    Added instructions to cpu.py

---
+++ src/cpu.py
@@ -0,0 +1,44 @@
+class CPU:
+    def __init__(self, memory):
+        accumulator = 0
+        current_address = 0
+        memory = memory
+
+    def run_instruction(operation, data):
+        pass
+
+    def read(data): # Tanner
+        pass
+
+    def write(data): # Tanner
+        pass
+
+    def load(data): # Tanner
+        pass
+
+    def store(data): # Frank
+        pass
+
+    def add(data): # Frank
+        pass
+
+    def subtract(data): # Frank
+        pass
+
+    def divide(data): # Kevin
+        pass
+
+    def multiply(data): # Kevin
+        pass
+
+    def branch(data): # Kevin
+        pass
+
+    def branchneg(data): # Noah
+        pass
+
+    def branchzero(data): # Noah
+        pass
+
+    def halt(data): # Noah
+        pass
