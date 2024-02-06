9715f718f1d0462169fb5269f860cc459c0f9395
Noah Christensen <mcnbc12@gmail.com>
2024-02-06 22:13:35 +0000

    Aded main boilerplate

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
 # todo: create function read program from text file
 # todo: create function to read program from command line
 
+from cpu import CPU
+
+
+def get_program_from_file():
+    pass
+
+def get_program_from_cli():
+    pass
+
+def parse_program(program: [str]) -> [int]:
+    pass
+
+def run_program(program):
+    d = CPU(program)
+    d.run_until_halt()
+
 def main():
     pass
 
